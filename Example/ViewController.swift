bddedb504ddcdc610dbeb5e745421daae5cb3a39
Sahand Raeisi <sahandraeisi1994@gmail.com>
2021-02-07 08:22:15 +0330

    Added example target

---
+++ Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by shndrs on 2/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
